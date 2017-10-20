130c6305f376103a04af390a975641b4501913ad
Mathew Calkins <mathewpcalkins@gmail.com>
2017-10-19 22:40:50 -0400

    more fiddling

--- src/app.js
+++ src/app.js
@@ -13,7 +13,47 @@ const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
   return contents.split('\n');
 };
-
 // TODO: your code to handle requests
 
+server.get('/greet-me', (req, res) => {
+  res.send('<h1>Hello!</h1>');
+});
+
+server.get('/', (req, res) => {
+  res.send('<h1>Home page</h1>');
+});
+
+server.get('/bigger-file', (req, res) => {
+  fs.readFile('index.html', 'utf8', (err, contents) => {
+    if (err) {
+      throw err;
+    }
+    res.send(contents);
+  });
+});
+
+server.get('/lesson-plan', (req, res) => {
+  const lessonPlan = {
+    title: 'Node.js and Express',
+    tagline: 'Server-side JavaScript'
+    // ...
+  };
+
+  // res.set('Content-type', 'application/json');
+  // res.type('json');
+  // res.send(JSON.stringify(lessonPlan));
+
+  res.json(lessonPlan);
+});
+
 server.listen(3000);
+
+
+// JSON = JavaScript Object Notation
+// A way to represent a JS object as a string
+// Convert object to string - "serialization"
+// Convert stirng back to ojbect - "deserialization"
+// JSON.stringify(object)
+// Example: JSON.stringify({ a: 3}) -> '{"a":3}'
+// const str = JSON.stringify({ a: 3})
+// JSON.parse(str) -> { a: 3 }
